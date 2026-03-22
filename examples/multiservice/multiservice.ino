d14f07d1d85524f084b5b9bf3850c4fdea43cfc4
Włodzimierz Ciesielski <vovagorodok2@gmail.com>
2026-03-22 01:49:36 +0100

    Added multiservice support for native ESP32

--- examples/multiservice/multiservice.ino
+++ examples/multiservice/multiservice.ino
@@ -33,6 +33,16 @@ public:
 LedCharacteristicCallbacks ledCallbacks;
 #endif
 
+#ifdef BLE_BATTERY_BLE_LIB_ESP32
+class LedCharacteristicCallbacks: public BLECharacteristicCallbacks {
+public:
+  void onWrite(BLECharacteristic* characteristic) override {
+    digitalWrite(LED_BUILTIN, *characteristic->getData());
+  }
+};
+LedCharacteristicCallbacks ledCallbacks;
+#endif
+
 MyBatteryService batteryService{};
 
 void setup() {
@@ -61,6 +71,21 @@ void setup() {
     NIMBLE_PROPERTY::READ | NIMBLE_PROPERTY::WRITE
   );
   ledCharacteristic->setValue(false);
+  ledCharacteristic->setCallbacks(&ledCallbacks);
+  ledService->start();
+  ArduinoBleBattery.begin(server, batteryService);
+  advertiseBle(server, NAME, BLE_BATTERY_SERVICE_UUID, LED_SERVICE_UUID);
+#endif
+
+#ifdef BLE_BATTERY_BLE_LIB_ESP32
+  auto server = initBle(NAME);
+  auto ledService = server->createService(LED_SERVICE_UUID);
+  auto ledCharacteristic = ledService->createCharacteristic(
+    LED_CHARACTERISTIC_UUID,
+    BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE
+  );
+  uint8_t value = 0;
+  ledCharacteristic->setValue(&value, sizeof(value));
   ledCharacteristic->setCallbacks(&ledCallbacks);
   ledService->start();
   ArduinoBleBattery.begin(server, batteryService);
--- src/ArduinoBleBatteryMultiservice.h
+++ src/ArduinoBleBatteryMultiservice.h
@@ -104,6 +104,9 @@ inline bool advertiseBle(BLEServer* server,
     #ifdef BLE_BATTERY_BLE_LIB_NIM_BLE_ARDUINO
     return advertising->start();
     #else
+    #ifdef BLE_BATTERY_BLE_LIB_ESP32
+    advertising->start();
+    #endif
     return true;
     #endif
 }
